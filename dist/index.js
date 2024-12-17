98d19c8f89e9d7d7db2c68fd53995056136d0862
sure12456 <surendra12456@gmail.com>
2024-12-17 18:40:35 +0530

    Issue with adding Arguments part - sovled

--- dist/index.js
+++ dist/index.js
@@ -10,9 +10,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const selenium_webdriver_1 = require("selenium-webdriver");
+const chrome_1 = require("selenium-webdriver/chrome");
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        let driver = yield new selenium_webdriver_1.Builder().forBrowser(selenium_webdriver_1.Browser.CHROME).build();
+        const options = new chrome_1.Options();
+        options.addArguments('--disable-blink-features=AutomationControlled');
+        let driver = yield new selenium_webdriver_1.Builder().forBrowser(selenium_webdriver_1.Browser.CHROME).setChromeOptions(options).build();
         try {
             yield driver.get('https://meet.google.com/yrm-pvbi-ohk?hs=193&hs=187&ijlm=1734417497293&adhoc=1');
             // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Builder, Browser, By, Key, until } from 'selenium-webdriver'
+import { Options } from 'selenium-webdriver/chrome'
 
 async function main() {
-  let driver = await new Builder().forBrowser(Browser.CHROME).build()
+
+  const options = new Options()
+  options.addArguments('--disable-blink-features=AutomationControlled')
+
+
+  let driver = await new Builder().forBrowser(Browser.CHROME).setChromeOptions(options).build()
   try {
     await driver.get('https://meet.google.com/yrm-pvbi-ohk?hs=193&hs=187&ijlm=1734417497293&adhoc=1')
     // await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
