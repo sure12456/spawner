d229c245c2bca8cf56dd137f60adcd693ba79a3d
sure12456 <surendra12456@gmail.com>
2024-12-25 21:58:38 +0530

    Error correction

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Options } from 'selenium-webdriver/chrome'
 
 async function openMeet(driver: WebDriver) {
   try {
-    await driver.get('https://meet.google.com/rgv-jfxg-zqn')
+    await driver.get('https://meet.google.com/uyt-owim-xzx')
 
     await driver.wait(until.elementLocated(By.xpath('//span[contains(text(), "Got it")]')), 10000).click();
     await driver.wait(until.elementLocated(By.id('c11')), 10000).sendKeys("Testing1");
@@ -35,76 +35,97 @@ async function getDriver() {
 
     function wait(delayInMS) {
       return new Promise((resolve) => setTimeout(resolve, delayInMS));
-    }
-
-    function startRecording(stream, lengthInMS) {
-      let recorder = new MediaRecorder(stream);
-      let data = [];
-
-      recorder.ondataavailable = (event) => data.push(event.data);
-      recorder.start();
-
-      let stopped = new Promise((resolve, reject) => {
-        recorder.onstop = resolve;
-        recorder.onerror = (event) => reject(event.name);
-      });
-
-
-      let recorded = wait(lengthInMS).then(() => {
-        if (recorder.state === 'recording') {
-          recorder.stop();
-        }
-      });
-
-    return Promise.all([stopped, recorded]).then(() => data);
-
   }
 
-  console.log("Before Entering mediaDevice")
+  function startRecording(stream, lengthInMS) {
+      let recorder = new MediaRecorder(stream);
+      let data = [];
+      
+      recorder.ondataavailable = (event) => data.push(event.data);
+      recorder.start();
+      
+      let stopped = new Promise((resolve, reject) => {
+          recorder.onstop = resolve;
+          recorder.onerror = (event) => reject(event.name);
+      });
+      
+      let recorded = wait(lengthInMS).then(() => {
+          if (recorder.state === "recording") {
+          recorder.stop();
+          }
+      });
+      
+      return Promise.all([stopped, recorded]).then(() => data);
+  }
 
+  console.log("before mediadevices")
   window.navigator.mediaDevices.getDisplayMedia({
-    video: {
-      displaySurface: "browser"
-    },
-    audio: false,
-    preferCurrentTab: true,
-  }).then(async stream => {
+      video: {
+        displaySurface: "browser"
+      },
+      audio: true,
+      preferCurrentTab: true
+  }).then(async screenStream => {                        
+      const audioContext = new AudioContext();
+      const screenAudioStream = audioContext.createMediaStreamSource(screenStream)
+      const audioEl1 = document.querySelectorAll("audio")[0];
+      const audioEl2 = document.querySelectorAll("audio")[1];
+      const audioEl3 = document.querySelectorAll("audio")[2];
+      const audioElStream1 = audioContext.createMediaStreamSource(audioEl1.srcObject)
+      const audioElStream2 = audioContext.createMediaStreamSource(audioEl3.srcObject)
+      const audioElStream3 = audioContext.createMediaStreamSource(audioEl2.srcObject)
 
-    // console.log("audio logic start");
-    // const audioContext = new AudioContext();
-    // const screenAudioStream = audioContext.createMediaStreamSource(stream)
-    // const audiosrc1 = document.querySelectorAll("audio")[0];
-    // const audiosrc2 = document.querySelectorAll("audio"")[1];
-    // const audiosrc3 = document.querySelectorAll("audio")[2];
+      const dest = audioContext.createMediaStreamDestination();
 
-    // const audioStream1 = audioContext.createMediaStreamSource(audiosrc1.srcObject);
-    // const audioStream2 = audioContext.createMediaStreamSource(audiosrc2.srcObject);
-    // const audioStream3 = audioContext.createMediaStreamSource(audiosrc3.srcObject);
+      screenAudioStream.connect(dest)
+      audioElStream1.connect(dest)
+      audioElStream2.connect(dest)
+      audioElStream3.connect(dest)
+
+      // window.setInterval(() => {
+      //   document.querySelectorAll("audio").forEach(audioEl => {
+      //     if (!audioEl.getAttribute("added")) {
+      //       console.log("adding new audio");
+      //       const audioEl = document.querySelector("audio");
+      //       const audioElStream = audioContext.createMediaStreamSource(audioEl.srcObject)
+      //       audioEl.setAttribute("added", true);
+      //       audioElStream.connect(dest)
+      //     }
+      //   })
+
+      // }, 2500);
     
-    // const dest = audioContext.createMediaStreamDestination();
-
-    // screenAudioStream.connect(dest);
-    // audioStream1.connect(dest);
-    // audioStream2.connect(dest);
-    // audioStream3.connect(dest);
-
-
-    console.log("Stream Started now")
-    const recordedChunks = await startRecording(stream, 10000);
-    console.log("Recording started")
-
+    // Combine screen and audio streams
+    const combinedStream = new MediaStream([
+        ...screenStream.getVideoTracks(),
+        ...dest.stream.getAudioTracks()
+    ]);
+    
+    console.log("before start recording")
+    const recordedChunks = await startRecording(combinedStream, 60000);
+    console.log("after start recording")
+    
     let recordedBlob = new Blob(recordedChunks, { type: "video/webm" });
+    
+    // Create download for video with audio
     const recording = document.createElement("video");
     recording.src = URL.createObjectURL(recordedBlob);
-
+    
     const downloadButton = document.createElement("a");
     downloadButton.href = recording.src;
-    downloadButton.download = "RecordedVideo.webm";
+    downloadButton.download = "RecordedScreenWithAudio.webm";    
     downloadButton.click();
-    console.log("Download button clicked")
-  }) 
+    
+    console.log("after download button click")
+    
+    // Clean up streams
+    screenStream.getTracks().forEach(track => track.stop());
+    audioStream.getTracks().forEach(track => track.stop());
+  })
+  
   `)
-  // await driver.sleep(600000)
+  console.log(response)
+  driver.sleep(600000)
 }
 
 
